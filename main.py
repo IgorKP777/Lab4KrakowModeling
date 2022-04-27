13923c1c4043436ed76d6681319568dfa3f1ef06
Admin <igorkydryashov7k@gmail.com>
2022-04-27 21:54:45 +0800

    upd

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import openpyxl as xl
 from prettytable import PrettyTable as tablePr
-from mmn import MMN
+from mmn import *
 
 
 # получаем данные для варианта из файла xlsx
@@ -20,34 +20,47 @@ def data(v: int):
     return lambdaV[v], mu[v], n[v], m[v]
 
 
-while 1:
-    # запрос варианта от пользователя
-    variant = int()
-    try:
-        variant = int(input('введите вариант '))
-        print()
-    except ValueError:
-        print('введите число')
-        exit(-1)
+def table_designations() -> tablePr:
+    """показатели эффективности СМО"""
+    table = tablePr()
+    table.title = 'показатели эффективности смо'.upper()
+    table.field_names = ['показатель', 'значение показателя']
+    table.align = 'l'
+    table.add_row(['Q', 'пропускная способность'])
+    table.add_row(['P(отк)', 'вероятность отказа в обслуживании'])
+    table.add_row(['P(оч)', 'вероятность появления очереди'])
+    table.add_row(['n_pod', 'среднее число занятых приборов'])
+    table.add_row(['m_pod', 'среднее число заявок в очереди'])
+    table.add_row(['N_pod', 'среднее число заявок в системе'])
+    table.add_row(['t_оч', 'среднее время нахождения заявки в очереди'])
+    table.add_row(['t_c', 'среднее время нахождения заявки в системе'])
+    return table
 
-    # проверка на номер варианта от 1 до 24
-    if not 1 <= variant <= 24:
-        print('введите номер варианта от 1 до 24')
-        exit(-2)
 
-    lam, mu, n, m = data(variant - 1)
-    inputData = tablePr()
-    inputData.title = 'исходные данные для варианта'
-    inputData.field_names = ['вариант', 'lambda', 'mu', 'n', 'm']
-    inputData.add_row(row=[variant, lam, mu, n, m])
-    inputData.align = 'c'
-    inputData.encoding = 'utf-8'
-    print(inputData)
-    print()
-    # вызов трех моделей и вывод в консоль в виде таблицы
-    models = MMN()
-    # models.mmn0(lam=lam, mu=mu, n=n, m=m)
-    # models.mmnm(lam=lam, mu=mu, n=n, m=m)
-    # models.mmn8(lam=lam, mu=mu, n=n, m=m)
-    models.mmn8(lam=2, mu=2, n=2, m=1)
-    print(models.tableResult())
+# запрос варианта от пользователя
+input_variant = input('введите номер варианта ')
+variant = int()
+if input_variant.isnumeric():
+    variant = int(input_variant)
+else:
+    print('введите число')
+    exit(-1)
+# проверка на номер варианта от 1 до 24
+if not 1 <= variant <= 24:
+    print('введите номер варианта от 1 до 24')
+    exit(-2)
+lam, mu, n, m = data(variant - 1)
+inputData = tablePr()
+inputData.title = 'исходные данные для варианта'.upper()
+inputData.field_names = ['вар.', 'λ', 'μ', 'n', 'm']
+inputData.add_row(row=[variant, lam, mu, n, m])
+inputData.align = 'c'
+inputData.encoding = 'utf-8'
+print(inputData)
+print()
+# вызов трех моделей и вывод в консоль в виде таблицы
+mmn0(lam=lam, mu=mu, n=n, m=m)
+mmnm(lam=lam, mu=mu, n=n, m=m)
+mmn8(lam=lam, mu=mu, n=n, m=m)
+print(table_designations(), '\n')
+print(tableResult())
--- mmn.py
+++ mmn.py
@@ -2,71 +2,81 @@ import math
 from prettytable import PrettyTable as tablePr
 
 table = tablePr()
-table.title = 'таблица результатов'
+table.title = 'таблица результатов'.upper()
 table.field_names = ['m', 'p(отк)', 'Q', 'n', 'kn', 'tc', 'сум. вер.']
 table.align = 'c'
 table.float_format = '.3'
 table.encoding = 'utf-8'
 
 
-class MMN:
+# модель MMN0
+def mmn0(lam, mu, n, m) -> None:
+    """модель MMN0"""
+    alfa = lam / mu
+    summa = float()
+    for k in range(1, n + 1):
+        a = (alfa ** k) / math.factorial(k)
+        summa += a
+    p0 = (1 + summa) ** -1
+    p = [p0]
+    for k in range(1, n + 1):
+        p.append((alfa ** k) / math.factorial(k) * p0)
+    p_otk = p[n]
+    q = lam * (1 - p_otk)
+    n_pod = alfa * (1 - p_otk)
+    t_c = n_pod / lam
+    table.add_row(['0', p_otk, q, n_pod, (n_pod / n) * 100, t_c, sum(p)])
 
-    # модель MMN0
-    def mmn0(self, lam, mu, n, m) -> None:
-        alfa = lam / mu
-        summa = float()
-        for k in range(1, n + 1):
-            a = (alfa ** k) / math.factorial(k)
-            summa += a
-        p0 = (1 + summa) ** -1
-        p = [p0]
-        for k in range(1, n + 1):
-            p.append((alfa ** k) / math.factorial(k) * p0)
-        p_otk = p[n]
-        q = lam * (1 - p_otk)
-        n_pod = alfa * (1 - p_otk)
-        t_c = n_pod / lam
-        table.add_row(['0', p_otk, q, n_pod, (n_pod / n) * 100, t_c, sum(p)])
 
-    # модель MMNM
-    def mmnm(self, lam, mu, n, m) -> None:
-        alfa = lam / mu
-        summa1 = float()
-        for k in range(1, n + 1):
-            summa1 += (alfa ** k) / math.factorial(k)
-        summa2 = float()
-        for s in range(1, m + 1):
-            summa2 += (alfa / n) ** s
-        p0 = (1 + summa1 + (alfa ** n) / math.factorial(n) * summa2) ** -1
-        p = [p0]
-        for k in range(1, n + 1):
-            p.append((alfa ** k) / math.factorial(k) * p0)
-        for s in range(1, m + 1):
-            p.append(((alfa ** n) / math.factorial(n)) * ((alfa / n) ** s) * p0)
-        p_otk = p[n + m]
-        q = lam * (1 - p_otk)
-        n_pod = alfa * (1 - p_otk)
-        m_pod = float()
-        for s in range(1, m + 1):
-            m_pod += s * p[n + s]
-        n_big = n_pod + m_pod
-        t_c = n_big / lam
-        table.add_row(['2', p_otk, q, n_pod, (n_pod / n) * 100, t_c, sum(p)])
+# модель MMNM
+def mmnm(lam, mu, n, m) -> None:
+    """модель MMNM"""
+    alfa = lam / mu
+    summa1 = float()
+    for k in range(1, n + 1):
+        summa1 += (alfa ** k) / math.factorial(k)
+    summa2 = float()
+    for s in range(1, m + 1):
+        summa2 += (alfa / n) ** s
+    p0 = (1 + summa1 + (alfa ** n) / math.factorial(n) * summa2) ** -1
+    p = [p0]
+    for k in range(1, n + 1):
+        p.append((alfa ** k) / math.factorial(k) * p0)
+    for s in range(1, m + 1):
+        p.append(((alfa ** n) / math.factorial(n)) * ((alfa / n) ** s) * p0)
+    p_otk = p[n + m]
+    q = lam * (1 - p_otk)
+    n_pod = alfa * (1 - p_otk)
+    m_pod = float()
+    for s in range(1, m + 1):
+        m_pod += s * p[n + s]
+    n_big = n_pod + m_pod
+    t_c = n_big / lam
+    table.add_row(['2', p_otk, q, n_pod, (n_pod / n) * 100, t_c, sum(p)])
 
-    # модель MMN∞
-    def mmn8(self, lam, mu, n, m) -> None:
-        alfa = lam / mu
-        summa1 = float()
-        for k in range(1, n + 1):
-            summa1 += (alfa ** k) / math.factorial(k)
-        p0 = (1 + summa1 + ((alfa ** (n + 1)) / (math.factorial(n) * (n - alfa)))) ** -1
-        p = [p0]
-        for k in range(1, n + 1):
-            p.append(((alfa ** k) / math.factorial(k)) * p0)
 
-        print(p, sum(p))
-        # table.add_row(['∞', pOch, q, nPod, (nPod / n) * 100, tc, '---'])
-        pass
+# модель MMN∞
+def mmn8(lam, mu, n, m) -> None:
+    """модель MMN∞"""
+    alfa = lam / mu
+    summa1 = float()
+    for k in range(1, n + 1):
+        summa1 += (alfa ** k) / math.factorial(k)
+    p0 = (1 + summa1 + ((alfa ** (n + 1)) / (math.factorial(n) * (n - alfa)))) ** -1
+    p = [p0]
+    for k in range(1, n + 1):
+        p.append(((alfa ** k) / math.factorial(k)) * p0)
+    for k in range(n + 1, n + 3):
+        p.append((alfa ** k) / (math.factorial(n) * n ** (k - n)) * p0)
+    p_och = 1 - sum(p[:-1])
+    n_pod = m
+    q = lam
+    m_pod = (m * p_och) / (n - m)
+    n_big = n_pod + m_pod
+    t_c = n_big / lam
+    t_och = m_pod / lam
+    table.add_row(['∞', 'p(оч) ' + str(round(p_och, 3)), q, n_pod, (n_pod / n) * 100, t_c, '---'])
 
-    def tableResult(self) -> tablePr:
-        return table
+
+def tableResult() -> tablePr:
+    return table
